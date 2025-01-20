0ae4c7221b3e7a7beef7c10dd56b8b8c083da267
Vlad K. <kpas95545@gmail.com>
2025-01-20 23:08:15 +0400

    fixes processing.py and widget.py

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,8 @@ from typing import Dict, List
 
 
 def filter_by_state(list_dict: List[Dict], opt_value: str = "EXECUTED") -> List[Dict]:
-    """Функция, которая возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению"""
+    """Функция, которая возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению"""
     new_list_dict = []
     for every_dict in list_dict:
         if every_dict["state"] == opt_value:
@@ -11,6 +12,6 @@ def filter_by_state(list_dict: List[Dict], opt_value: str = "EXECUTED") -> List[
 
 
 def sort_by_date(list_dict: List[Dict], arg_sort: bool = True) -> List[Dict]:
-    """Функция, которая возвращает новый список, отсортированный по дате(date)"""
+    """Функция, которая возвращает новый список, отсортированный по дате"""
     sort_list = sorted(list_dict, key=lambda every_dict: every_dict["date"], reverse=arg_sort)
     return sort_list
--- src/widget.py
+++ src/widget.py
@@ -4,13 +4,13 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_and_number: Union[str]) -> Union[str]:
-    """ Функция, которая маскирует номер счета или карты"""
-    if 'Счет' in type_and_number:
-        return f'Счет {get_mask_account(type_and_number[-6:])}'
+    """Функция, которая маскирует номер счета или карты"""
+    if "Счет" in type_and_number:
+        return f"Счет {get_mask_account(type_and_number[-6:])}"
     else:
-        return f'{type_and_number[:-16]} {get_mask_card_number(type_and_number[-16:])}'
+        return f"{type_and_number[:-16]} {get_mask_card_number(type_and_number[-16:])}"
 
 
 def get_date(data: str) -> str:
     """Функция которая возвращает строку в нужном формате"""
-    return f'{data[8:10]}.{data[5:7]}.{data[0:4]}'
+    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
