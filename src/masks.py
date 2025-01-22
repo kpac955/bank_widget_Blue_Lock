9f9df7825fc7f3780d3a5353b95c0395c2ee50bc
Vlad K. <kpas95545@gmail.com>
2025-01-23 01:23:45 +0400

    fixes masks.py, widget.py, update pyproject.toml and poetry.lock

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ from typing import Union
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Прописываем функцию маскировки номера банковской карты клиента"""
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]} "
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
