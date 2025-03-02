83f6ccb02069cb46eddb9315a679c7200b91709c
kotbk <kpas95545@gmail.com>
2025-03-02 22:48:26 +0400

    update generators.py and conftest.py, add test_generators,py

--- src/generators.py
+++ src/generators.py
@@ -11,3 +11,19 @@ def filter_by_currency(transaction_list: list[dict], code_of_currency: str) -> I
         transaction_list,
     )
     return filtred_transactions
+
+
+def transaction_descriptions(transaction_list: list[dict]) -> Iterator:
+    """Функция возвращает описания для транзакций"""
+    for transaction in transaction_list:
+        yield transaction["description"]
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """Генератор номеров карт формата 'ХХХХ ХХХХ ХХХХ ХХХХ' в заданном числовом диапозоне"""
+    if not isinstance(start, (int | float)) or not isinstance(stop, (int | float)):
+        raise TypeError ("Ошибка типа данных")
+    for x in range(start, stop + 1):
+        card_number = f"{x:016}"
+        formatted_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        yield formatted_number
