7d272666d9fdbb481928ba8456e6adb14302287c
kotbk <kpas95545@gmail.com>
2025-05-21 20:51:39 +0400

    update function "convert" in module external_api.py, tests for external_api.py, report html tests

--- src/external_api.py
+++ src/external_api.py
@@ -9,31 +9,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
-
 API_KEY = os.getenv("API_KEY")
 
 
 def convert(transactions: Dict[str, object]) -> float:
     # Выполняет перевод суммы транзакции в рубли
     # Если USD, EUR, то идет обращение к API сервису
-    amount = float(transactions.get("amount", 0))
-    currency = str(transactions.get("currency", "RUB")).upper()
+    operation = transactions.get("operationAmount", {})
+    amount = float(operation.get("amount", 0))
+    currency = str(operation.get("currency", {}).get("code", "RUB")).upper()
     # Если транзакция в рублях, то функция просто возвращает данную сумму
     if currency == "RUB":
         return amount
     # Если транзакция не в RUB, EUR, USD  возвращаем 0.0
     if currency not in ("USD", "EUR"):
         return 0.0
-    else:
-        headers = {"apikey": API_KEY}
 
-        params = {"base": currency, "symbols": "RUB"}
+    headers = {"apikey": API_KEY}
+
+    params = {"base": currency, "symbols": "RUB"}
 
     response = requests.get(URL, headers=headers, params=params)
     data = response.json()
-    rate = data.get("rates").get("RUB")
+    result = data.get("result")
 
-    if rate is None:
+    if result is None:
         return 0.0
 
-    return amount * rate
+    return result
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -6,18 +6,21 @@ from src.external_api import convert
 @patch("src.external_api.requests.get")
 def test_convert(mock_get):
     mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = {"rates": {"RUB": 80}}
-
+    mock_get.return_value.json.return_value = {"result": 400.0}
     # Создаем конвертацию валюты для
     # USD
-    usd = {"amount": 5, "currency": "USD"}
+    usd = {"operationAmount": {"amount": 5, "currency": {"name": "USD", "code": "USD"}}}
     assert convert(usd) == 400.0
+
+    mock_get.return_value.json.return_value = {"result": 1600.0}
     # EUR
-    eur = {"amount": 20, "currency": "EUR"}
+    eur = {"operationAmount": {"amount": 20, "currency": {"name": "EUR", "code": "EUR"}}}
     assert convert(eur) == 1600.0
+
     # RUB
-    rub = {"amount": 4500, "currency": "RUB"}
+    rub = {"operationAmount": {"amount": "4500", "currency": {"name": "RUB", "code": "RUB"}}}
     assert convert(rub) == 4500.0
-    # при неизвестной валюте получаем - 0.0
-    nn = {"amount": 200, "currency": "NN"}
+
+    # Не определена
+    nn = {"operationAmount": {"amount": "200", "currency": {"name": "None", "code": "NN"}}}
     assert convert(nn) == 0.0
