84902b5d04ef81fa8578b3f4ebd6f9820b41391a
kotbk <kpas95545@gmail.com>
2025-05-23 00:05:50 +0400

    update external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -15,9 +15,28 @@ API_KEY = os.getenv("API_KEY")
 def convert(transactions: Dict[str, object]) -> float:
     # Выполняет перевод суммы транзакции в рубли
     # Если USD, EUR, то идет обращение к API сервису
+
+    # получаем ключ из словаря transactions
+    # если его нет или что то не является словарем - возвращаем ноль
     operation = transactions.get("operationAmount", {})
-    amount = float(operation.get("amount", 0))
-    currency = str(operation.get("currency", {}).get("code", "RUB")).upper()
+    if not isinstance(operation, dict):
+        return 0.0
+
+    # извлекаем "amount" из словаря operation и приводим ее к float
+    # при неверном значении выводим ноль
+    amount_to = operation.get("amount", "0")
+    try:
+        amount = float(amount_to)
+    except(ValueError, TypeError):
+        return 0.0
+    # извлекаем "currency" из словаря operation
+    # если не словарь - возвращаем 0.0
+    currency_data = operation.get("currency", {})
+    if not isinstance(currency_data, dict):
+        return 0.0
+
+    currency = str(currency_data.get("code", "RUB")).upper()
+
     # Если транзакция в рублях, то функция просто возвращает данную сумму
     if currency == "RUB":
         return amount
@@ -26,14 +45,15 @@ def convert(transactions: Dict[str, object]) -> float:
         return 0.0
 
     headers = {"apikey": API_KEY}
-
-    params = {"base": currency, "symbols": "RUB"}
-
-    response = requests.get(URL, headers=headers, params=params)
-    data = response.json()
-    result = data.get("result")
-
-    if result is None:
+    params = {
+        "from": currency,
+        "to": "RUB",
+        "amount": amount
+    }
+    try:
+        response = requests.get(URL, headers=headers, params=params)
+        data = response.json()
+        result = data.get("result")
+        return float(result) if result is not None else 0.0
+    except (requests.RequestException, ValueError, TypeError):
         return 0.0
-
-    return result
